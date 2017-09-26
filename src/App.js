10f599a16830ecbaf8b08dbd45ebbf7dc3c3842f
Wouter de Vos <wouter@codaisseur.com>
2017-09-26 14:55:00 +0200

    Rendering the Recipes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import Title from './components/Title'
-import './App.css'
+import RecipesContainer from './recipes/RecipesContainer'
+import recipes from './fixtures/recipes'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Title content="All Recipes" />
-        <Title content="A Spanish Omelette" />
+      <div>
+        <RecipesContainer recipes={ recipes } />
       </div>
-    );
+    )
   }
 }
 
