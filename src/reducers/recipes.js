b6783fce4252f86194af419624c1f23a90216893
Wouter de Vos <wouter@codaisseur.com>
2017-09-27 16:24:12 +0200

    Create Recipe Action

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -1,10 +1,14 @@
 import { SEED_RECIPES } from '../actions/recipes/seed'
 import { UPDATE_RECIPE } from '../actions/recipes/update'
+import { CREATE_RECIPE } from '../actions/recipes/create'
 
 export default (state = [], { type, payload } = {}) => {
   switch (type) {
     case SEED_RECIPES :
       return state.concat(payload)
+
+    case CREATE_RECIPE :
+      return [{ ...payload }].concat(state)
 
     case UPDATE_RECIPE :
       return state.map((recipe) => {
